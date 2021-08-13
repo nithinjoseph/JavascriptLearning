fc960a7904a6806d6afc6aba54d2a7f69bb424a8
nithinjoseph <nith.joseph@gmail.com>
2021-08-13 14:55:03 +0530

    Calculator functionel

--- main.js
+++ main.js
@@ -1,46 +1,62 @@
-const outputList = document.getElementById("outputList");
-const btn = document.querySelector(".btn-primary");
-let persons = [];
+let haveDot = false;
+let operationE1= document.querySelectorAll(".operation");
+let history = document.getElementById("history");
+let firstNum = null;
+let operaterEle= null;
+let secondNum=null;
+let disp =null;
+//Displaying
+const display=(val)=>{
+  if (val==='.'&& !haveDot){
+    haveDot=true;
+  }else if(val==='.'&& haveDot){
+    return;
+  }
+  document.getElementById("result").value +=val;
+  document.getElementById("history").innerHTML+= val;
+ }
+ //Clearing
+const clearCall = () => {
+  document.getElementById("result").value="";
+  document.getElementById("history").innerHTML="";
+ }
 
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  btnSubmission();
-});
-function btnSubmission() {
-  const firstName = document.getElementById("firstName");
-  const lastName = document.getElementById("lastName");
-  const emailEntry = document.getElementById("emailEntry");
-  const addressEntry = document.getElementById("addressEntry");
-  if (firstName.value === "") {
-    firstName.classList.add("is-invalid");
-  }
-  if (lastName.value === "") {
-    lastName.classList.add("is-invalid");
-  }
-  if (emailEntry.value === "") {
-    emailEntry.classList.add("is-invalid");
-  }
-  if (addressEntry.value === "") {
-    addressEntry.classList.add("is-invalid");
-  } else {
-    addItem();
-  }
+ //Operator Detection
+ operationE1.forEach(opertaion=>{
+  opertaion.addEventListener("click", (e)=>{
+    operaterEle= e.target.value;
+    firstNum =document.getElementById("result").value.slice(0, -1);
+    document.getElementById("result").value="";
+    haveDot=false;
+  })
+ });
 
-  function addItem() {
-    let person = {
-      firstName: document.getElementById("firstName").value,
-      lastName: document.getElementById("lastName").value,
-      emailEntry: document.getElementById("emailEntry").value,
-      addressEntry: document.getElementById("addressEntry").value,
-    };
-    persons.push(person);
-    document.forms[0].reset();
 
-    let ouputListItem = document.createElement("li");
-    ouputListItem.classList.add("card", "p-2", "mt-2", "mb-2");
-    for (let i = 0; i < persons.length; i++) {
-      ouputListItem.innerHTML = `<div>First name: <b> ${persons[i].firstName}</b> </div><div>Last name:<b>${persons[i].lastName} </b></div><div>Email:<b>${persons[i].emailEntry} </b></div><div>Address:<b>${persons[i].addressEntry}</b></div>`;
+ //Calculating
+const solve =()=>{
+    secondNum = document.getElementById("result").value;
+    console.log(`${firstNum} ${operaterEle} ${secondNum} `);
+
+    switch(operaterEle){
+        case "+":
+          document.getElementById("result").value= firstNum + secondNum;
+          alert("addition");
+          break;
+        case "-":
+          document.getElementById("result").value= firstNum - secondNum;
+          alert("Substarction");
+          break;
+        case "*":
+          document.getElementById("result").value= firstNum * secondNum;
+          alert("Multiplication");
+          break;
+        case "/":
+        document.getElementById("result").value= firstNum / secondNum;
+        alert("Division");
+        break;  
     }
-    outputList.appendChild(ouputListItem);
-  }
+
 }
+
+
+
